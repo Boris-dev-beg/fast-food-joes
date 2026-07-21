75650fbce32a162a92e50869476bc9904dd47449
Mangwa Boris <borisweteneyi1@gmail.com>
2026-07-21 03:44:26 +0100

    Refining the Header and add Footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,5 +16,19 @@ function responsive_menu() {
         menu === null || menu === void 0 ? void 0 : menu.classList.add("translate-x-0");
         menu === null || menu === void 0 ? void 0 : menu.classList.remove("-translate-x-full");
     };
+    const menuLinks = document.querySelectorAll("nav a");
+    menuLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+            const currRef = link.getAttribute("href");
+            menuLinks.forEach((navLink) => {
+                const otherRef = navLink.getAttribute("href");
+                navLink.classList.remove("active");
+                if (currRef === otherRef)
+                    navLink.classList.add("active");
+            });
+            link.classList.add("active");
+            Close();
+        });
+    });
 }
 // responsive_menu()
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,23 +1,37 @@
-window.addEventListener("load",()=>{
-    responsive_menu()
-})
+window.addEventListener("load", () => {
+  responsive_menu();
+});
 
-function responsive_menu(){
-    const btn_close = document.getElementById("btn-close") as HTMLButtonElement
-    const btn_open = document.getElementById("btn-open") as HTMLButtonElement
-    const menu = document.getElementById("responsive-menu") as HTMLElement
+function responsive_menu() {
+  const btn_close = document.getElementById("btn-close") as HTMLButtonElement;
+  const btn_open = document.getElementById("btn-open") as HTMLButtonElement;
+  const menu = document.getElementById("responsive-menu") as HTMLElement;
 
-    btn_close.addEventListener("click", () => Close())
-    btn_open.addEventListener("click", () => Open())
+  btn_close.addEventListener("click", () => Close());
+  btn_open.addEventListener("click", () => Open());
 
- const Close = () => {
-    menu?.classList.add("-translate-x-full")
-    menu?.classList.remove("translate-x-0")
- }   
- const Open = () => {
-    menu?.classList.add("translate-x-0")
-    menu?.classList.remove("-translate-x-full")
- }   
+  const Close = () => {
+    menu?.classList.add("-translate-x-full");
+    menu?.classList.remove("translate-x-0");
+  };
+  const Open = () => {
+    menu?.classList.add("translate-x-0");
+    menu?.classList.remove("-translate-x-full");
+  };
+
+  const menuLinks = document.querySelectorAll("nav a");
+
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      const currRef = link.getAttribute("href");
+      menuLinks.forEach((navLink) => {
+        const otherRef = navLink.getAttribute("href");
+        navLink.classList.remove("active");
+
+        if (currRef === otherRef) navLink.classList.add("active");
+      });
+      link.classList.add("active");
+      Close();
+    });
+  });
 }
-
-// responsive_menu()
